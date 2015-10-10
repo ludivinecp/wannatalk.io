3ed72c977d2e496a977955749d37a438f14dc3db
Valerie Chatelet <vlrchtlt@gmail.com>
2015-10-10 18:56:46 +0200

    Participant generated with scaffold

---
+++ db/migrate/20151010165355_change_protection_to_participant.rb
@@ -0,0 +1,9 @@
+class ChangeProtectionToParticipant < ActiveRecord::Migration
+  def up
+    change_column_null(:participants, :name, false)
+  end
+
+  def down
+    change_column_null(:participants, :name, true)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151010164434) do
+ActiveRecord::Schema.define(version: 20151010165355) do
 
   create_table "conferences", force: :cascade do |t|
     t.string   "title",      null: false
     t.string   "url"
     t.date     "date",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "participants", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

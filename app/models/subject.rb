d5b109e74ba7294ac481365b2800fdff322ae7b5
Valerie Chatelet <vlrchtlt@gmail.com>
2015-10-14 10:11:23 +0200

    fixed validation in Subject

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,5 +1,8 @@
 class Subject < ActiveRecord::Base
   validates :title, presence: true
+  validates :participant, presence: true
+  validates :conference, presence: true
+
   belongs_to :participant
   belongs_to :conference
 end

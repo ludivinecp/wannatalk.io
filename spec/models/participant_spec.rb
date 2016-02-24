0754a8c36c6198eff447e0c14b4e9a01fe300715
ludivinecp <coipel.ludivine@gmail.com>
2016-02-24 14:57:04 +0100

    implementing test => stoped on error /uninitialized constant Participants/ when running test for model participant

--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
-describe Participant do
+RSpec.describe Participant, type: :model do
   it "has a valid factory"
+  	# Factory.create(:participant).should be_valid
   it "is invalid without a name"
   it "returns a participant's full name as a string"
 end

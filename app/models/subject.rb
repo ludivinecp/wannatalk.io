bf40dce4171da99fa7faba727c87a91ae24c2a21
ludivinecp <coipel.ludivine@gmail.com>
2016-02-29 16:08:26 +0100

    gem shoulda matchers added

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,8 +1,5 @@
 class Subject < ActiveRecord::Base
-  validates :title, presence: true
-  validates :participant_id, presence: true
-  validates :conference_id, presence: true
-  validates :description, presence: true
+  validates_presence_of  :title, :participant_id, :conference_id, :description
 
   has_many :participants, through: :subject_participants, :class_name => "Participant", :foreign_key => "interested_id"
   belongs_to :participant, :class_name => "Participant", :foreign_key => "questioner_id"

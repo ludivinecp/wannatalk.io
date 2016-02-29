379382dd1dca621e1c4b14a0c27d2b80cab42c77
ludivinecp <coipel.ludivine@gmail.com>
2016-02-29 15:14:01 +0100

    factory girl installed + test models writed for participants, subjects and conferences

--- app/models/subject_participant.rb
+++ app/models/subject_participant.rb
@@ -1,4 +1,5 @@
 class SubjectParticipant < ActiveRecord::Base
 	belongs_to :participant, :class_name => "Participant", :foreign_key => "interested_id"
     belongs_to :subject
+    validates_presence_of :participant_id, :conference_id
 end
--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
 RSpec.describe Participant, type: :model do
-  it "has a valid factory"
-  	Factory.create(:participant).should be_valid
-  it "is invalid without a name"
-  it "returns a participant's full name as a string"
+  it "has a valid factory" do
+  	FactoryGirl.create(:participant).should be_valid
+  end 
+  it "is invalid without a name" do
+ 	 FactoryGirl.build(:participant, name: nil).should_not be_valid
+  end
 end

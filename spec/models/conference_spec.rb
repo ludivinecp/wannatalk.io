e3646f54759f5a0acfd399184aff61d8cb00270d
ludivinecp <coipel.ludivine@gmail.com>
2016-03-04 15:24:09 +0100

    conference spec ok

--- spec/models/conference_spec.rb
+++ spec/models/conference_spec.rb
@@ -1,13 +1,26 @@
-require 'spec_helper'
+require 'rails_helper'
 
-RSpec.describe Conference, type: :model do
+# RSpec.describe Conference, type: :model do
+#   it "has a valid factory" do
+#   	FactoryGirl.create(:conference).should be_valid
+#   end 
+#   it "is invalid without a title" do
+#  	 FactoryGirl.build(:conference, title: nil).should_not be_valid
+#   end
+#   it "is invalid without a date" do
+#  	 FactoryGirl.build(:conference, date: nil).should_not be_valid
+#   end
+# end
+
+describe Conference do
+
   it "has a valid factory" do
-  	FactoryGirl.create(:conference).should be_valid
-  end 
-  it "is invalid without a title" do
- 	 FactoryGirl.build(:conference, title: nil).should_not be_valid
+  FactoryGirl.create(:conference).should be_valid
   end
-  it "is invalid without a date" do
- 	 FactoryGirl.build(:conference, date: nil).should_not be_valid
-  end
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:date) }
+
+  it { should have_many(:subjects) } 
+
 end
+

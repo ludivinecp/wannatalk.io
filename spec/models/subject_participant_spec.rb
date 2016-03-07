38f13b7fe890e1a95f3935f6c1e8c2b3b2755981
ludivinecp <coipel.ludivine@gmail.com>
2016-03-07 01:31:32 +0100

    fix tests after changes FK

--- spec/models/subject_participant_spec.rb
+++ spec/models/subject_participant_spec.rb
@@ -1,15 +1,15 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe SubjectParticipant do
+describe SubjectParticipant do
 
-#   it "has a valid factory" do
-#   FactoryGirl.create(:subject_participant).should be_valid
-#   end
-#   it { is_expected.to validate_presence_of(:participant_id) }
-#   it { is_expected.to validate_presence_of(:subject_id) }
+  it "has a valid factory" do
+  FactoryGirl.create(:subject_participant).should be_valid
+  end
+  it { is_expected.to validate_presence_of(:interested_id) }
+  it { is_expected.to validate_presence_of(:subject_id) }
 
-#   it { should belong_to(:interested) }
-#   it { should belong_to(:subject) }
+  it { should belong_to(:interested) }
+  it { should belong_to(:subject) }
 
-# end
+end
 

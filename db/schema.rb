bacb9d98c1b295e27c4e84de3b53bf523f973c85
ludivinecp <coipel.ludivine@gmail.com>
2016-03-08 10:11:02 +0100

    remove useless index from tables subject and subject_participant

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160307131918) do
+ActiveRecord::Schema.define(version: 20160308090654) do
 
   create_table "conference_participants", force: :cascade do |t|
     t.integer  "participant_id"
@@ -54,9 +54,6 @@ ActiveRecord::Schema.define(version: 20160307131918) do
     t.datetime "updated_at",    null: false
   end
 
-  add_index "subject_participants", ["interested_id"], name: "index_subject_participants_on_interested_id"
-  add_index "subject_participants", ["subject_id"], name: "index_subject_participants_on_subject_id"
-
   create_table "subjects", force: :cascade do |t|
     t.string   "title",         null: false
     t.text     "description"
@@ -65,8 +62,5 @@ ActiveRecord::Schema.define(version: 20160307131918) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
-
-  add_index "subjects", ["conference_id"], name: "index_subjects_on_conference_id"
-  add_index "subjects", ["questioner_id"], name: "index_subjects_on_questioner_id"
 
 end

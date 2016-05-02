5bcee50cb907c4f4616bd34066360e8493b8c9bf
ludivinecp <coipel.ludivine@gmail.com>
2016-05-02 16:18:05 +0200

    participant is able to valid his participation to a subject

--- app/controllers/subject_participant_controller.rb
+++ app/controllers/subject_participant_controller.rb
@@ -7,19 +7,15 @@ class SubjectParticipantController < ApplicationController
   def create
   @subject_participant = SubjectParticipant.new(subject_participant_params)
 
-    respond_to do |format|
-      if @subject_participant.save
-        format.html { redirect_to @subject, notice: 'Participation was successfully created.' }
-        format.json { render :show, status: :created, location: @subject_participant }
-      else
-        format.html { render :new }
-        format.json { render json: @subject_participant.errors, status: :unprocessable_entity }
-      end
+    if @subject_participant.save
+      redirect_to subjects_path, notice: 'Votre participation a été enregistré'
+    else
+      redirect_to subjects_path
     end
   end
 
   def subject_participant_params
-    params.require(:subject_participant).permit(:interested, :subject)
+    params.permit(:interested_id, :subject_id)
   end
 
 end

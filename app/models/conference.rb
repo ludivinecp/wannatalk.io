a6ab712d67387d5851cb0319e12976395ae5172f
ludivinecp <coipel.ludivine@gmail.com>
2016-05-03 14:37:20 +0200

    add dependent destroy to model

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -1,7 +1,7 @@
 class Conference < ActiveRecord::Base
   validates_presence_of :title, :date
-  has_many :subjects
-  has_many :conference_participants
-	has_many :participants, through: :conference_participants
+  has_many :subjects, :dependent => :destroy
+  has_many :conference_participants, :dependent => :destroy
+  has_many :participants, through: :conference_participants
 
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,7 +1,7 @@
 class Subject < ActiveRecord::Base
   validates_presence_of  :title, :conference, :description, :questioner
 
-  has_many :subject_participants
+  has_many :subject_participants, :dependent => :destroy
   has_many :interested, through: :subject_participants, :class_name => "Participant", :foreign_key => "interested_id" #interested
   belongs_to :questioner, :class_name => "Participant" #questioner
   belongs_to :conference

b7c1e84bf8c96f1186cff10233e2f524ceb6f28a
ludivinecp <coipel.ludivine@gmail.com>
2016-06-17 13:29:46 +0200

    fix associations

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,7 +2,7 @@ class Subject < ActiveRecord::Base
   validates_presence_of  :title, :questioner, :conference, :description
 
   has_many :subject_participants
-  has_many :interested, through: :subject_participants, :class_name => "Participant", :foreign_key => "interested_id" #interested
-  belongs_to :questioner, :class_name => "Participant" #questioner
+  has_many :interested, through: :subject_participants #interested
+  belongs_to :questioner, class_name: "Participant"
   belongs_to :conference
 end

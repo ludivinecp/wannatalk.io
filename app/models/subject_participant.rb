adaf922cf86c888292cac88f12e96a2145e5bcab
ludivinecp <coipel.ludivine@gmail.com>
2016-06-20 11:37:59 +0200

    add validation for uniqueness participation per subject

--- app/models/subject_participant.rb
+++ app/models/subject_participant.rb
@@ -1,5 +1,6 @@
 class SubjectParticipant < ActiveRecord::Base
-  validates_presence_of :interested_id, :subject_id
+  validates :interested_id, presence: true 
+	validates :subject_id, presence: true, uniqueness: { scope: :interested_id}
 	belongs_to :interested, :class_name => "Participant" #interested
   belongs_to :subject
 end

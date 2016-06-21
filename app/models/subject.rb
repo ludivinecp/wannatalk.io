b36d866f71a0800a74fc44a30af5ec2c3f9086e4
ludivinecp <coipel.ludivine@gmail.com>
2016-06-21 14:51:39 +0200

    refacto button

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -5,4 +5,8 @@ class Subject < ActiveRecord::Base
   has_many :interested, through: :subject_participants #interested
   belongs_to :questioner, class_name: "Participant"
   belongs_to :conference
+
+  def participation(current_participant) 
+  	self.subject_participants.find_by_interested_id(current_participant.id)
+  end
 end

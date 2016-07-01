01570ec8ecb26f6106e154929b9a039544b9ab5c
ludivinecp <coipel.ludivine@gmail.com>
2016-07-01 16:21:15 +0200

    save paris rb conferences from the api

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -4,17 +4,36 @@ class Conference < ActiveRecord::Base
   has_many :conference_participants
 	has_many :participants, through: :conference_participants
 
-  def self.save_conference_from_api
+  def data_from_api
     response = ApiMeetup.new.events('parisrb')
     api_data = JSON.parse(response.body)
-      conferences = api_data.map do |line|
-        next if line['name'].nil? ||line['time'].nil? || !line['name'].include?('ParisRb.new(premier: :mardi, pour: :talks)')
-        conference = Conference.new
-        conference.title = line['name']
-        conference.date = line['time']
-        conference.url = line['link']
-        conference.save!
+
+    api_data
+  end
+
+  #Keep only Paris RB conferences
+  def parisrb_conferences(api_data)
+    parisrb = []
+    api_data.each do |event|
+      if event["name"] == "ParisRb.new(premier: :mardi, pour: :talks)"
+        parisrb << event
       end
-    Conference.all
+    end
+    parisrb
+  end
+
+  #Save Paris RB conferences from the Meetup API
+  def self.save_conference_from_api
+    data = Conference.new.data_from_api
+    Conference.new.parisrb_conferences(data).map do |line|
+      conference = Conference.new
+      conference.title = line['name']
+      conference.date = line['time']
+      conference.url = line['link']
+      conference.save!
+    end
+   Conference.all
   end
 end
+
+
